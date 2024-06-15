cc96a62e6373243489a1e5b0afed3082e3096da5
rex930524 <a0938821055@gmail.com>
2024-06-16 02:06:42 +0800

    Update piano.js

--- pianosounds/piano.js
+++ pianosounds/piano.js
@@ -1,7 +1,7 @@
 document.addEventListener('keydown', function(event){
 	const audio = document.querySelector(`audio[data-key = "${event.keyCode}"]`);
 	const key = document.querySelector(`div[data-key = "${event.keyCode}"]`);
-	key.volume=1;
+	key.volume=0.2;
 	if(!audio) return; 
 	
 	audio.currentTime = 0;
